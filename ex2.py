38060160cb3f845d3a59c2b31281df187eb3238b
Carvs10 <39344056+Carvs10@users.noreply.github.com>
2019-10-24 22:50:32 -0300

    First update to program routine

---
+++ ex2.py
@@ -0,0 +1,7 @@
+ray = float(input())
+pi = 3.14159
+area = (ray ** 2) * pi
+
+print("A={:.4f}".format(area))
+
+
---
+++ ex8.py
@@ -0,0 +1,6 @@
+num = int(input())
+hours = int(input())
+value = float(input())
+
+print('NUMBER = {}'.format(num))
+print('SALARY = U$ {:.2f}'.format(hours*value))
